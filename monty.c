e23de75b80735e05c0d83c16f21038b2b7424e8d
Fayth7 <flexflakes@gmail.com>
2023-05-26 01:43:31 -0700

    main

--- monty.c
+++ monty.c
@@ -9,51 +9,65 @@
  */
 int main(int argc, char *argv[])
 {
-char *filename;
-char line[256];
-int line_number = 1;
-char *opcode;
-char *argument;
-FILE *file;
-if (argc != 2)
-{
-printf("USAGE: monty file\n");
-return (EXIT_FAILURE);
-}
-filename = argv[1];
-file = fopen(filename, "r");
-if (file == NULL)
-{
-printf("Error: Can't open file %s\n", filename);
-return (EXIT_FAILURE);
-}
-while (fgets(line, sizeof(line), file))
-{
-line[strcspn(line, "\n")] = '\0';
-opcode = strtok(line, " ");
-if (opcode == NULL)
-{
-line_number++;
-continue;
-}
-argument = strtok(NULL, " ");
-if (strcmp(opcode, "push") == 0)
-{
-if (argument == NULL)
-{
-printf("L%d: usage: push integer\n", line_number);
-fclose(file);
-return (EXIT_FAILURE);
-}
-}
-else
-{
-printf("L%d: unknown instruction %s\n", line_number, opcode);
-fclose(file);
-return (EXIT_FAILURE);
-}
-line_number++;
-}
-fclose(file);
-return (EXIT_SUCCESS);
+    char *filename;
+    char line[256];
+    int line_number = 1;
+    char *opcode;
+    char *argument;
+    FILE *file;
+    stack_t *stack = NULL;
+
+    if (argc != 2)
+    {
+        printf("USAGE: monty file\n");
+        return EXIT_FAILURE;
+    }
+
+    filename = argv[1];
+    file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        printf("Error: Can't open file %s\n", filename);
+        return EXIT_FAILURE;
+    }
+
+    while (fgets(line, sizeof(line), file))
+    {
+        line[strcspn(line, "\n")] = '\0';
+        opcode = strtok(line, " ");
+        if (opcode == NULL)
+        {
+            line_number++;
+            continue;
+        }
+
+        argument = strtok(NULL, " ");
+
+        if (strcmp(opcode, "push") == 0)
+        {
+int value;
+	  if (argument == NULL)
+            {
+                printf("L%d: usage: push integer\n", line_number);
+                fclose(file);
+                return EXIT_FAILURE;
+            }
+            value = atoi(argument);
+            push(&stack, value);
+        }
+        else if (strcmp(opcode, "pall") == 0)
+        {
+            pall(&stack);
+        }
+        else
+        {
+            printf("L%d: unknown instruction %s\n", line_number, opcode);
+            fclose(file);
+            return EXIT_FAILURE;
+        }
+        line_number++;
+    }
+
+    fclose(file);
+    return EXIT_SUCCESS;
 }
